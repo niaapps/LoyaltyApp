14a90839023c9451949ecd130d213b01b71908d9
niaapps <niaapplications@gmail.com>
2019-06-10 13:56:30 -0400

    Biz and Cust object classes

---
+++ app/src/main/java/com/niaapplications/loyaltyapp/Business.java
@@ -0,0 +1,35 @@
+package com.niaapplications.loyaltyapp;
+
+public class Business {
+
+
+ private String name, email, pw, street, state, city,zip, number;
+
+
+    public Business(){
+        this.name = name;
+        this.email = email;
+        this.number = number;
+        this.city = city;
+        this.pw = pw;
+        this.street = street;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Business(String name, String email, String pw, String street, String state, String city, String zip, String number){
+
+        this.name = name;
+        this.email = email;
+        this.number = number;
+        this.city = city;
+        this.pw = pw;
+        this.street = street;
+        this.state = state;
+        this.zip = zip;
+
+
+
+    }
+}
+
---
+++ app/src/main/java/com/niaapplications/loyaltyapp/Customer.java
@@ -0,0 +1,20 @@
+package com.niaapplications.loyaltyapp;
+
+public class Customer {
+
+private String usrNumber, usrEmail;
+private int points = 0;
+
+
+public Customer(){
+    this.usrNumber = usrNumber;
+    this.usrEmail = usrEmail;
+    this.points = points;
+}
+    public Customer(String num, String email, int points){
+        this.usrNumber = num;
+        this.usrEmail = email;
+        this.points = points;
+    }
+
+}
